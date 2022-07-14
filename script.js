a4a11a6a3346ffcac9ba059cb4ef48c519f6a8a6
Bryant Young <thebryisonfire@gmail.com>
2022-07-14 07:24:20 -1000

    updated project card styling

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function onLoad() {
-  window.addEventListener("scroll", showNav);
+  //window.addEventListener("scroll", showNav);
   renderProjects();
 }
 
@@ -12,16 +12,18 @@ function showNav() {
   }
 }
 
-function addProject(title, codeLink, previewLink) {
+function addProject(imgSrc, title, codeLink, previewLink) {
   const projectsContainer = document.querySelector(".projects-container");
 
   const projectCard = document.createElement("div");
 
   projectCard.innerHTML = `
+  <img src="${imgSrc}" alt="">
   <h3>${title}</h3>
-  <hr>
+  <div class="button-container">
   <a href="${codeLink}"><button class="card-btn">View Code</button></a>
   <a href="${previewLink}"><button class="card-btn">Live Demo</button></a>
+  <div>
 `;
 
   projectsContainer.appendChild(projectCard);
@@ -29,28 +31,33 @@ function addProject(title, codeLink, previewLink) {
 
 function renderProjects() {
   addProject(
+    "images/restaurant-ss.png",
     "Sample Restaurant Page made with Webpack and vanilla JS",
     "https://github.com/lifeofbry19/restaurant-page",
     "https://lifeofbry19.github.io/restaurant-page/"
   );
   addProject(
+    "images/library-ss.png",
     "Personal Library with Local Storage",
     "https://github.com/lifeofbry19/js-library",
     "https://lifeofbry19.github.io/js-library/"
   );
   addProject(
+    "images/signupform-ss.png",
     "Sample Signup Form with basic validation",
     "https://github.com/lifeofbry19/sample-signup-form",
     "https://lifeofbry19.github.io/sample-signup-form/"
   );
 
   addProject(
+    "images/calculator-ss.png",
     "Calculator App built with vanilla JS",
     "https://github.com/lifeofbry19/js-calculator",
     "https://lifeofbry19.github.io/js-calculator/"
   );
 
   addProject(
+    "images/tictactoe-ss.png",
     "Tic Tac Toe game built with vanilla JS",
     "https://github.com/lifeofbry19/tic-tac-toe",
     "https://lifeofbry19.github.io/tic-tac-toe/"
